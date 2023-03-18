b8bd634fdb3a6bc900a621aa8703ed132cf83d13
Raton Biswas <raton35-2241@diu.edu.bd>
2023-03-18 16:39:18 +0600

    pure react components with props👍👌

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "LUNA"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanse"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.bread),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Buddy",
+      animal: "Cat",
+      bread: "Bread",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      bread: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Papers",
+      animal: "Bird",
+      bread: "Cocktail",
+    }),
   ]);
 };
 
