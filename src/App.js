586be68344be9ab236df935f4e251273dd33f968
Raton Biswas <raton35-2241@diu.edu.bd>
2023-03-18 16:34:18 +0600

    pure react components❤

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-const pet = ()=> {
-    return React.createElement("div",{},[
-        React.createElement("h1", {}, "LUNA"),    
-        React.createElement("h2", {}, "Dog"),    
-        React.createElement("h2", {}, "Havanse")    
-    ]
-    );
-}
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "LUNA"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanse"),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt Me!")
-    );
-  };
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
 
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
