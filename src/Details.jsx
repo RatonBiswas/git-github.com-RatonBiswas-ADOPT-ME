ca4574dd574c4c1d96943ac6cf8246d4506fdd69
Raton Biswas <raton35-2241@diu.edu.bd>
2023-04-13 13:24:16 +0600

    portals-and-refs

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
-
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -21,20 +23,31 @@ const Details = () => {
 
   return (
     <div className="details">
-    <Carousel images={pet.images} />
-    <div>
-      <h1>{pet.name}</h1>
-      <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-      <button>Adopt {pet.name}</button>
-      <p>{pet.description}</p>
+      <Carousel images={pet.images} />
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+      </div>
     </div>
-  </div>
   );
 };
 
 function DetailsErrorBoundary(props) {
   return (
-    <ErrorBoundary >
+    <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
   );
