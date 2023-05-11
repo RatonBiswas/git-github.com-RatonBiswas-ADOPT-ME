3f1a7c324adf7d001024fede23575855340511e5
Raton Biswas <raton35-2241@diu.edu.bd>
2023-05-12 02:49:55 +0600

    node-streams-with-express

--- src/App.jsx
+++ src/App.jsx
@@ -47,20 +47,23 @@
 // const root = createRoot(container);
 // root.render(<App />);
 
-
 //import { createRoot } from "react-dom/client";
-import {  Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+//import Details from "./Details";
+//import SearchParams from "./SearchParams";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true,
     },
   },
 });
@@ -69,21 +72,27 @@ const App = () => {
   const adoptedPet = useState(null);
   return (
     <div>
-      
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+              </div>
+            }
+          >
             <header>
               <Link to="/">Adopt Me!</Link>
             </header>
             <Routes>
-              <Route path="/details/:id" element={<Details />} />
+              <Route path="/details/:id" element={<Details/>} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-     
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </div>
   );
 };
 
-export default App
+export default App;
